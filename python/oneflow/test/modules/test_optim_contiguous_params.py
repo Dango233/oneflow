bc9a196f4cb5c9ac61f0e99b6d6724a76df767dc
songyicheng <int.rejoice@gmail.com>
2022-09-28 01:40:56 +0000

    fix the bug of referenced Tensor.grad updating

--- python/oneflow/test/modules/test_optim_contiguous_params.py
+++ python/oneflow/test/modules/test_optim_contiguous_params.py
@@ -16,139 +16,61 @@ limitations under the License.
 
 import unittest
 from collections import OrderedDict
-import os
 import numpy as np
-from oneflow.test_utils.test_util import GenArgDict
+from copy import deepcopy
+import pdb
 
-import oneflow as flow
-from oneflow.nn.parameter import Parameter
+import oneflow
+from oneflow import nn
+from oneflow.test_utils.test_util import GenArgDict
 from oneflow.nn.optimizer.contiguous_params import ContiguousParams
 
 
-def compare_with_numpy_sgd(
-    test_case, device, x_shape, tensor_num, weight_decay, learning_rate, train_iters
+def test_equal_optimizer_update(
+    test_case, device, x_shape, param_num, weight_decay, learning_rate, train_iters
 ):
-    random_grad_seq = []
-    init_value_seq = []
+    xx = []
+    for i in range(train_iters):
+        xx.append(np.random.uniform(size=x_shape).astype(np.float32))
+    yy = np.random.uniform(size=x_shape).astype(np.float32)
+    ce = nn.CrossEntropyLoss()
 
-    for _ in range(train_iters):
-        random_grad_seq_per_iter = []
-        for i in range(tensor_num):
-            random_grad_seq_per_iter.append(
-                np.random.uniform(size=x_shape).astype(np.float32)
-            )
-        random_grad_seq.append(random_grad_seq_per_iter)
+    model_ref = nn.Sequential(*[nn.Linear(x_shape[-1], 
+                            x_shape[-1]) for i in range(param_num)])
+    model_ref = model_ref.to(device)
+    optimizer = oneflow.optim.SGD(model_ref.parameters(), lr=learning_rate, 
+                                    weight_decay=weight_decay)
+    
+    model_c = deepcopy(model_ref)
+    parameters_c = ContiguousParams(model_c.parameters())
+    optimizer_c = oneflow.optim.SGD(parameters_c.contiguous(), lr=learning_rate,
+                                    weight_decay=weight_decay)
 
-    for i in range(tensor_num):
-        init_value_seq.append(np.random.uniform(size=x_shape).astype(np.float32))
-
-    def train_by_oneflow():
-        x_tensor_list = []
-        for i in range(tensor_num):
-            x_tensor_list.append(
-                flow.Tensor(init_value_seq[i], device=flow.device(device))
-            )
-        lr_tensor = flow.Tensor(learning_rate, device=flow.device(device))
-
-        def train_one_iter(grad):
-            grad_tensor_list = []
-            for i in range(tensor_num):
-                grad_tensor_list.append(
-                    flow.tensor(
-                        grad[i],
-                        dtype=flow.float32,
-                        requires_grad=False,
-                        device=flow.device(device),
-                    )
-                )
-
-            flow._C.multi_tensor_sgd_update(
-                x_tensor_list, grad_tensor_list, lr_tensor, 1.0, weight_decay
-            )
-
+    for model, optimizer in zip([model_ref, model_c], [optimizer, optimizer_c]):
         for i in range(train_iters):
-            train_one_iter(random_grad_seq[i])
-        return x_tensor_list
-
-    def train_by_contiguous_param():
-        x_tensor_list = []
-        for i in range(tensor_num):
-            x_tensor_list.append(
-                flow.Tensor(init_value_seq[i], device=flow.device(device))
-            )
-        x_c = ContiguousParams(x_tensor_list)
-
-        sgd = flow.optim.SGD(
-            [
-                {
-                    "params": [x_c.contiguous()],
-                    "lr": learning_rate,
-                    "weight_decay": weight_decay,
-                }
-            ]
-        )
-
-        def train_one_iter(grad):
-            grad_tensor = flow.tensor(
-                grad,
-                dtype=flow.float32,
-                requires_grad=False,
-                device=flow.device(device),
-            )
-            loss = flow.sum([xi * grad_tensor for xi in x_c.original()])
+            x = oneflow.tensor(xx[i], device=device)
+            y = oneflow.tensor(yy, device=device)
+            #print(list(model.parameters())[0][0])
+            loss = ce(model(x), y)
+            optimizer.zero_grad()
             loss.backward()
-            sgd.clip_grad()
-            sgd.step()
-            sgd.zero_grad()
+            optimizer.step()
 
-        for i in range(train_iters):
-            train_one_iter(random_grad_seq[i])
+    for p1, p2 in zip(model_ref.parameters(), model_c.parameters()):
+        test_case.assertTrue(np.allclose(p1.numpy(), p2.numpy(), atol=1e-06))
 
-        x_c.assert_buffer_is_valid()
-
-        return x_c.original()
-
-    def train_by_numpy():
-        x = init_value_seq
-
-        def train_one_iter(grad):
-            for i in range(tensor_num):
-                grad[i] = grad[i] + weight_decay * x[i]
-                x[i] = x[i] - learning_rate * grad[i]
-            return x
-
-        for i in range(train_iters):
-            x = train_one_iter(random_grad_seq[i])
-        return x
-
-    oneflow_res = train_by_oneflow()
-    numpy_res = train_by_numpy()
-
-    for i in range(tensor_num):
-        test_case.assertTrue(
-            np.allclose(
-                oneflow_res[i].numpy().flatten(),
-                numpy_res[i].flatten(),
-                rtol=0.0001,
-                atol=0.0001,
-            )
-        )
-
-
-@unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
-@flow.unittest.skip_unless_1n1d()
-class TestOptimizers(flow.unittest.TestCase):
+@oneflow.unittest.skip_unless_1n1d()
+class Test(oneflow.unittest.TestCase):
     def test_multi_tensor_sgd_update(test_case):
         arg_dict = OrderedDict()
-        arg_dict["device"] = ["cuda"]
-        arg_dict["x_shape"] = [(2,)]
-        arg_dict["tensor_num"] = [4]
+        arg_dict["device"] = ["cpu", "cuda"]
+        arg_dict["x_shape"] = [(1, 8), (1, 20)]
+        arg_dict["param_num"] = [10, 20] # too few params may cause the gradient to disappear
         arg_dict["weight_decay"] = [0.0, 0.5]
         arg_dict["learning_rate"] = [1.0, 1e-3]
-        arg_dict["train_iters"] = [10]
+        arg_dict["train_iters"] = [5]
         for arg in GenArgDict(arg_dict):
-            compare_with_numpy_sgd(test_case, **arg)
-
+            test_equal_optimizer_update(test_case, **arg)
 
 if __name__ == "__main__":
     unittest.main()
